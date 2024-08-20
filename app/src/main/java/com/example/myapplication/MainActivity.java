6c92594f4a3c20e246bc32d09725c17f4b61e4b6
c-degni <christivanndeg@gmail.com>
2024-08-20 11:06:08 -0400

    Hello World Android App Setup

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    String msg = "Android: ";
+
+}
